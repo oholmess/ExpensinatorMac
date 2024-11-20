98f49ff1b984ba29044bd18f2a66cfbf8e750315
oholmess <oholmes.ieu2023@student.ie.edu>
2024-11-20 16:12:31 +0100

    Initial Commit

---
+++ ExpensinatorMac/ExpensinatorMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensinatorMacApp.swift
+//  ExpensinatorMac
+//
+//  Created by Oliver Holmes on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpensinatorMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpensinatorMacTests/ExpensinatorMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensinatorMacTests.swift
+//  ExpensinatorMacTests
+//
+//  Created by Oliver Holmes on 11/20/24.
+//
+
+import Testing
+@testable import ExpensinatorMac
+
+struct ExpensinatorMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
